9319521c50be14587deefd31e4f10b6418fd9996
hyob <gyqls0309j@naver.com>
2018-03-07 07:50:16 +0900

    Test 부분 수정

--- src/test/java/com/hyob/web/ApplicationTests.java
+++ src/test/java/com/hyob/web/ApplicationTests.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class WebApplicationTests {
+@SpringBootTest(classes = {Application.class})
+public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
